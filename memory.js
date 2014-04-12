82e55fd549ccab32c0e7f798ebf7849a7dc69b87
Ian McLaughlin <Ian.McLaughlin@razorfish.com>
2014-04-12 12:59:39 -0400

    memory v1 WIP

---
+++ memory.js
@@ -0,0 +1,26 @@
+$(function() {
+	
+	var count;
+	
+	
+	$('a').on('click', function(e) {
+		e.preventDefault;
+		
+		$(this).removeClass('hidden');
+		
+		count++;
+		
+	})
+	
+	var evaluateTiles = function(tile1, tile2) {
+		var tile1color = 
+	};
+	var matchTiles = function(tile1, tile2) {
+		
+	};
+	var resetTiles = function(tile1, tile2) {
+		
+	}; 
+	
+	
+});
